e8e3f98f8df76679e36ddf2baadeb3fe27ccadb6
Cristen Anderson <1cristen1@gmail.com>
2016-01-08 17:37:02 -0800

    Added template from GNU page

--- lab1-cristenSunnie/main.c
+++ lab1-cristenSunnie/main.c
@@ -1,15 +1,81 @@
 //lab1 Cristen, Sunnie CS111 Winter 2016
 
 //In Lab 1a, you'll warm up by implementing just the options --rdonly, --wronly, --command, and --verbose.
-#include<unistd.h>
-int main(){
+#include <stdio.h>     /* for printf */
+#include <stdlib.h>    /* for exit */
+#include <getopt.h>
 
-	// rdonly
+int
+main(int argc, char **argv)
+{
+    int c;
+    int digit_optind = 0;
 
-	// wronly
+   while (1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"add",     required_argument, 0,  0 },
+            {"append",  no_argument,       0,  0 },
+            {"delete",  required_argument, 0,  0 },
+            {"verbose", no_argument,       0,  0 },
+            {"create",  required_argument, 0, 'c'},
+            {"file",    required_argument, 0,  0 },
+            {0,         0,                 0,  0 }
+        };
 
-	// command
+       c = getopt_long(argc, argv, "abc:d:012",
+                 long_options, &option_index);
+        if (c == -1)
+            break;
 
-	// verbose
+       switch (c) {
+        case 0:
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+                printf(" with arg %s", optarg);
+            printf("\n");
+            break;
 
+       case '0':
+        case '1':
+        case '2':
+            if (digit_optind != 0 && digit_optind != this_option_optind)
+              printf("digits occur in two different argv-elements.\n");
+            digit_optind = this_option_optind;
+            printf("option %c\n", c);
+            break;
+
+       case 'a':
+            printf("option a\n");
+            break;
+
+       case 'b':
+            printf("option b\n");
+            break;
+
+       case 'c':
+            printf("option c with value '%s'\n", optarg);
+            break;
+
+       case 'd':
+            printf("option d with value '%s'\n", optarg);
+            break;
+
+       case '?':
+            break;
+
+       default:
+            printf("?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+
+   if (optind < argc) {
+        printf("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf("%s ", argv[optind++]);
+        printf("\n");
+    }
+
+   exit(EXIT_SUCCESS);
 }
