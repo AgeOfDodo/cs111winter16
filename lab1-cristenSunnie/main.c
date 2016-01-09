06df4a745e315ac28187d352580766f0aea542b0
Sunnie So <sunnieso3741@ucla.edu>
2016-01-08 16:41:39 -0800

    Add main.c and change folder name.

---
+++ lab1-cristenSunnie/main.c
@@ -0,0 +1,7 @@
+//lab1 Cristen, Sunnie CS111 Winter 2016
+
+//In Lab 1a, you'll warm up by implementing just the options --rdonly, --wronly, --command, and --verbose.
+#include<unistd.h>
+int main(){
+
+}
