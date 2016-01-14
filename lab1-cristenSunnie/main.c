af01194860fc9c4db738a27990d8d635891528ff
Sunnie So <sunnieso3741@ucla.edu>
2016-01-14 01:17:17 -0800

    create pipe()

--- lab1-cristenSunnie/main.c
+++ lab1-cristenSunnie/main.c
@@ -11,8 +11,8 @@ See README for further information
 #include <ctype.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-
-// Check if a file descriptor is valid
+#include <string.h>
+// [command] Check if a file descriptor is valid
 int validFd(int fd, int fd_array_cur){
 	if( fd >= fd_array_cur){	
   		fprintf(stderr, "Error: Invalid use of file descriptor %d before initiation.\n", fd);
@@ -21,7 +21,7 @@ int validFd(int fd, int fd_array_cur){
   	return 1;
 }
 
-// Check if the open system call had an error
+// [read write] Check if the open system call had an error
 int checkOpenError(int fd) {
 	if (fd == -1) {
 		fprintf(stderr, "Error: open returned unsuccessfully\n");
@@ -30,7 +30,7 @@ int checkOpenError(int fd) {
   return 0;
 }
 
-// Checks for --command arguments
+// [command] Checks for --command arguments
 int passChecks(char* str, int index, int num_args) {
   int i = 0;
   // checks if is a digit
@@ -49,6 +49,8 @@ int passChecks(char* str, int index, int num_args) {
   return 1;
 }
 
+
+//[command]
 // Stores all arguments for a command in args_array, until the next "--" flag
 // Doesn't include optarg which is accepted automatically.
 // Keeps the current index updated and returns the updated argv index (optind)
@@ -95,6 +97,8 @@ int main(int argc, char **argv) {
 
   // open flag
   int oflag = 0;
+  char oflag_str[1000];
+  strcpy(oflag_str, "");
 
   // Verbose can be on or off, automatically set to off
   int verbose = 0;
@@ -103,13 +107,10 @@ int main(int argc, char **argv) {
   while (1) {
     int option_index = 0;
     static struct option long_options[] = {
-    	// { "name",      has_arg,         *flag, val }
-        {"rdonly",      required_argument,  0,  'r' },
-        {"wronly",      required_argument,  0,  'w' },
-        {"command",     required_argument,  0,  'c' },
-        {"verbose",     no_argument,        0,  'v' },
+// SUBCOMMAND
+    	  {"command",     required_argument,  0,  'c' },
         {"wait",        no_argument,        0,  'z' },
-   //  file flags
+// FILE FLAGS
         {"append",      no_argument,        0,  6 },
         {"cloexec",     no_argument,        0,  7 },
         {"creat",       no_argument,        0,  8 },
@@ -121,7 +122,21 @@ int main(int argc, char **argv) {
         {"rsync",       no_argument,        0,  14 },
         {"sync",        no_argument,        0,  15 },
         {"trunc",       no_argument,        0,  16 },
-
+// FILE OPENING
+        {"rdonly",      required_argument,  0,  17 },
+        {"wronly",      required_argument,  0,  18 },
+        {"rdwr",        required_argument,  0,  19 },
+        {"pipe",        no_argument,        0,  20 },
+ 
+// MISCELLANEOUS  
+        {"verbose",     no_argument,        0,  21 },
+        {"profile",     no_argument,        0,  22 },
+        {"abort",       no_argument,        0,  23 },
+        {"catch",       required_argument,  0,  24 },
+        {"ignore",      required_argument,  0,  25 },
+        {"default",     required_argument,  0,  26 },
+        {"pause",       no_argument,        0,  27 },
+        
     };
 
     // get the next option
@@ -138,48 +153,7 @@ int main(int argc, char **argv) {
 
     switch (c) {
     
-    case 'r': // read only 
-    case 'w': // write only
-   		// assign oflag
-      if (c == 'r') 	oflag = O_RDONLY | oflag;
-   		else 			oflag = O_WRONLY | oflag;
-      
-      // find all arguments for the current flag
-      optind = findArgs(args_array, args_array_size, optind, &args_array_cur,
-                        argc, argv);
-
-      // print command if verbose is enabled
-      if (verbose) {
-        char * flags;
-        if (c == 'r') flags = "--rdonly";
-        else flags = "--wronly";
-        printf("%s ", flags);
-        printf("%s ", optarg);
-        for (j = 0; j < args_array_cur; j++) {
-          printf("%s ", args_array[j]);
-        }
-        printf("\n");
-      }
-      
-      // open file into read write file descriptor
-      int rw_fd = open(optarg, oflag, 777);
-      if(checkOpenError(rw_fd) == -1) {
-        exit_status = 1;
-        continue;
-      }
-      
-      // save file descriptor to array
-      if (fd_array_cur == fd_array_size) {
-      	fd_array_size *= 2;
-      	fd_array = (int*)realloc((void*)fd_array, fd_array_size); 
-      }
-      fd_array[fd_array_cur] = rw_fd;
-      fd_array_cur++;
-
-      //clean oflag content.
-      oflag = 0;
-      break;   
-      
+//command
     case 'c': { // command (format: --command i o e cmd args_array)
       int i, o, e; // stdin, stdout, stderr
 
@@ -243,11 +217,7 @@ int main(int argc, char **argv) {
       }
       break;
     }
-     
-    case 'v': // verbose
-      verbose = 1;
-      break;
-      
+//wait
     case 'z': {
       int status;
       //wait any child process to finish. 0 is for blocking.
@@ -267,40 +237,141 @@ int main(int argc, char **argv) {
      
 //append
     case 6:
-      oflag |= O_APPEND;
+      oflag |= O_APPEND;  strcat(oflag_str,"--append ");
     break;
 //cloexev
     case 7:
+      oflag |= O_CLOEXEC;  strcat(oflag_str,"--cloexev ");
     break;
 //creat
     case 8:
+      oflag |= O_CREAT;  strcat(oflag_str,"--creat ");
     break;
 //directory
     case 9:
+      oflag |= O_DIRECTORY;  strcat(oflag_str,"--directory ");
     break;
 //dsync
     case 10:
+      oflag |= O_DSYNC;  strcat(oflag_str,"--dsync ");
     break;
 //excl
     case 11:
+      oflag |= O_EXCL;  strcat(oflag_str,"--excl ");
     break;
 //nofollow
     case 12:
+      oflag |= O_NOFOLLOW;  strcat(oflag_str,"--nofollow ");
     break;
 //nonblock
     case 13:
+      oflag |= O_NONBLOCK;  strcat(oflag_str,"--nonblock ");
     break;
 //rsync
     case 14:
+      oflag |= O_RSYNC;  strcat(oflag_str,"--rsync ");
     break;
 //sync
     case 15:
+      oflag |= O_SYNC;  strcat(oflag_str,"--sync ");
     break;
 //trunc
     case 16:
+      oflag |= O_TRUNC;  strcat(oflag_str,"--trunc ");
     break;
+//read only
+    case 17: 
+//write only
+    case 18:
+//read and write 
+    case 19: 
+      // assign oflag
+      if (c == 17)      oflag = O_RDONLY | oflag;
+      else if( c== 18)  oflag = O_WRONLY | oflag;
+      else              oflag = O_RDWR | oflag;
+      // find all arguments for the current flag
+      optind = findArgs(args_array, args_array_size, optind, &args_array_cur,
+                        argc, argv);
+
+      // print command if verbose is enabled
+      if (verbose) {
+        printf("%s", oflag_str);
+        oflag_str[0] = '\0';
+        char * flags;
+        if (c == 17)    flags = "--rdonly";
+        else if(c == 18)flags = "--wronly";
+        else            flags = "--rdwr";
+
+        printf("%s ", flags);
+        printf("%s ", optarg);
+        for (j = 0; j < args_array_cur; j++) {
+          printf("%s ", args_array[j]);
+        }
+        printf("\n");
+      }
+      
+      // open file into read write file descriptor
+      int rw_fd = open(optarg, oflag, 777);
+      if(checkOpenError(rw_fd) == -1) {
+        exit_status = 1;
+        continue;
+      }
+      
+      // save file descriptor to array
+      if (fd_array_cur == fd_array_size) {
+        fd_array_size *= 2;
+        fd_array = (int*)realloc((void*)fd_array, fd_array_size); 
+      }
+      fd_array[fd_array_cur] = rw_fd;
+      fd_array_cur++;
+
+      //clean oflag content.
+      oflag = 0;
+      break;   
+//pipe
+    case 20:
+      if(verbose) printf("--pipe\n");
+      int pipefd[2]; //store indices of fd_array
+      if(pipe(pipefd)== -1){
+        fprintf(stderr, "Error: fail to create pipe. pipe() returns -1.\n");
+      }
+      // save file descriptor to array
+      if (fd_array_cur  + 1 >= fd_array_size) {
+        fd_array_size *= 2;
+        fd_array = (int*)realloc((void*)fd_array, fd_array_size); 
+      }
+      fd_array[fd_array_cur++] = pipefd[0];
+      fd_array[fd_array_cur++] = pipefd[1];
 
 
+      break;
+
+
+//verbose
+    case 21: 
+      verbose = 1;
+      break;
+//profile
+    case 22:
+
+      break;
+//abort
+    case 23:
+      if(verbose) printf("--abort\n");
+      abort();
+      break;
+//catch
+    case 24:
+      break;
+//ignore
+    case 25:
+      break;
+//default
+    case 26:
+      break;
+//pause    
+    case 27:
+      break;
     // case '?': // ? returns when doesn't recognize option character
       // break;
 
